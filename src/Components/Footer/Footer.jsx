d9cc3440374a62039329e24bb778d63c42a9fb24
ercanakalar1995 <05210000922@ogrenci.ege.edu.tr>
2022-12-01 18:42:21 +0300

    edit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="footer-row">
           <div className="col-1-of-2">
             <div className="footer__navigation">
-              <div style={{ height: '10vh', width: '50%' }}>
+              <div style={{ height: '8vh', width: '87%' }}>
                 <div id="map-container-google-2" className="map-container">
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d270.4551826345257!2d32.81764227828308!3d39.922348307808974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34580ac4a4ee1%3A0xa59a441b71b6849e!2zTEFSQSBNw5xIRU5ExLBTTMSwSw!5e0!3m2!1str!2str!4v1668593280302!5m2!1str!2str"
